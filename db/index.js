77cf4375e856a0d65ed83a2dd724b61bc9a92900
Hassan-Ali0 <hassanali031935@gmail.com>
2025-11-07 15:48:06 +0500

    add get request

--- db/index.js
+++ db/index.js
@@ -2,7 +2,12 @@ import mongoose from "mongoose";
 import 'dotenv/config'
 
 const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}.7w9v4hs.mongodb.net/${process.env.DB_NAME}?appName=HassanCluster`;
-
-mongoose.connect(url);
+(async () => {
+    try {
+        await mongoose.connect(url);
+    } catch (error) {
+        console.error(error.message)
+    }
+})()
 
 export default mongoose;
--- routes/productRoutes/get.js
+++ routes/productRoutes/get.js
@@ -1,7 +1,26 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import Product from '../../models//product/index.js';
 
-const getProducts = (req, res) => {
-    res.send("Hello World! this is get request");
+
+const getProducts = async (req, res) => {
+    try {
+        const products = await Product.find();
+        console.log(products)
+        res.status(200).send({
+            message: "users successfully fetched",
+            products: products,
+            total: products.length,
+            status: 200,
+        })
+    } catch (err) {
+        res.status(500).send({
+            message: err.message,
+            products: [],
+            total: 0,
+            status: 500,
+        })
+    }
 }
 
 
