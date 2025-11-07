70261c18a0917d89d7dd62777d36d64188f5adef
Hassan-Ali0 <hassanali031935@gmail.com>
2025-11-07 16:21:07 +0500

    add put request

--- routes/productRoutes/put.js
+++ routes/productRoutes/put.js
@@ -1,7 +1,24 @@
 import express from 'express';
+import Product from '../../models/product/index.js';
 
-const putProducts = (req, res) => {
-    res.send("Hello World! this is put request");
+const putProducts = async (req, res) => {
+    try {
+        const { id } = req.params
+        const product = await Product.findByIdAndUpdate(id, req.body)
+        res.status(201).send({
+            message: "product successfully updated",
+            product: product,
+            status: 201
+        });
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({
+            message: err.message,
+            product: null,
+            status: 500
+        });
+
+    }
 }
 
 
